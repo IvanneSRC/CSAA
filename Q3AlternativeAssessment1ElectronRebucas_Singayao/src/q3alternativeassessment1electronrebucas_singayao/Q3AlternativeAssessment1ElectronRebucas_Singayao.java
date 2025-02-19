955247b622f43c82c708d85b1c0acea24d1dcbe6
IvanneSRC <143993105+IvanneSRC@users.noreply.github.com>
2025-02-20 07:55:14 +0800

    Add files via upload

---
+++ Q3AlternativeAssessment1ElectronRebucas_Singayao/src/q3alternativeassessment1electronrebucas_singayao/Q3AlternativeAssessment1ElectronRebucas_Singayao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package q3alternativeassessment1electronrebucas_singayao;
+import javax.swing.JFrame;
+/**
+ *
+ * @author User
+ */
+public class Q3AlternativeAssessment1ElectronRebucas_Singayao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        IMTSElectronRebucas_Singayao Electron = new IMTSElectronRebucas_Singayao();
+        
+        Electron.show();
+    }
+    
+}
